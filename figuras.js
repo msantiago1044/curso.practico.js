5298273a9ca47ec59f794b6db4c4b98b889bcd48
msantiago1044 <97839381+msantiago1044@users.noreply.github.com>
2022-05-22 18:27:38 -0500

    ok

--- figuras.js
+++ figuras.js
@@ -1,1 +1,18 @@
-console.log ("hola world")
+// codigo del cuadrado
+
+const ladoCuadrado = 5;
+const perimetroCuadrado = (ladoCuadrado * 4 + " cm");
+const areaCuadrado = (ladoCuadrado * ladoCuadrado + " cm2");
+
+console.log (areaCuadrado)
+
+
+// codigo del triangulo
+
+const ladoTriangulo1 = 4;
+const ladoTriangulo2 = 5;
+const ladoTriangulo3 = 5;
+const perimetroTriangulo = (ladoTriangulo1 + ladoTriangulo2 + ladoTriangulo3 + " cm");
+const areaTriangulo = ((Math.sqrt(Math.trunc(Math.pow(ladoTriangulo2) - Math.pow(ladoTriangulo1 / 2))) * ladoTriangulo1) / 2 + " cm2");
+
+console.log (areaTriangulo)
